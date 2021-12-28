ee4492a0ac53b5b2a62ab7072e308cfd27e2ab36
nixx78 <nixx78@gmail.com>
2021-12-28 15:09:15 +0200

    Logger added to project

--- hello-docker/readme.md
+++ hello-docker/readme.md
@@ -3,6 +3,9 @@ _docker build -t nixx/hello-docker ._
 
  - Run Docker:
 _docker run --name HelloDocker -d -p 8080:8080 nixx/hello-docker_
+ - 
+ - Run Docker with volume mapper 
+_docker run --name HelloDocker -v /tmp/logs:/logs -d -p 8080:8080 nixx/hello-docker_
 
  - Stop Docker:
 _docker stop HelloDocker_
@@ -12,3 +15,8 @@ _docker rm HelloDocker_
 
  - Active list
 _docker ps > t_
+
+ - Console for Application
+_docker exec -it HelloDocker /bin/sh_
+
+
--- hello-docker/src/main/java/lv/nixx/poc/hellodocker/HelloDockerApplication.java
+++ hello-docker/src/main/java/lv/nixx/poc/hellodocker/HelloDockerApplication.java
@@ -1,5 +1,7 @@
 package lv.nixx.poc.hellodocker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloDockerApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloDockerApplication.class);
+
+    // https://spring.io/guides/gs/spring-boot-docker/
+    // https://docs.docker.com/compose/gettingstarted/
+
+    // Setup logger && How to check log files ?!
+
     @RequestMapping("/hello")
     public String home() {
-        return "Hello Docker World";
+        LOG.info("Request from client");
+        return "Hello Docker World:" + System.currentTimeMillis();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloDockerApplication.class, args);
     }
+
 
 }
